cd984fa03aa0f3d72c2fce16da135ea80f9508b7
Shaamini Prinja <shaaminiprinja@Shaaminis-MacBook-Air>
2024-01-24 10:02:25 +0000

    addition of summary tables, changing headings on data set, moving data to be more cohesive

--- jupiter.py
+++ jupiter.py
@@ -13,10 +13,17 @@ class Moons:
         connectable = f"sqlite:///jupiter.db"
         query = "SELECT * FROM moons"
         
-        jupiter_data = pd.read_sql(query, connectable)
+        read_df = pd.read_sql(query, connectable)
+        jupiter_data = read_df.rename(columns={'period_days': 'Period (days)', 
+                                               'distance_km': 'Distance (km)',
+                                               'radius_km': 'Radius (km)', 
+                                               'mag': 'Mangnitude',
+                                               'group' : "Group", 
+                                               "mass_kg" : "Mass (kg)",
+                                               "ecc" : "Eccentricity", 
+                                               "inclination_deg" : "Inlination Degree"})
         jupiter_data.set_index("moon", inplace=True)
         self.jupiter_data = jupiter_data
-        
         return jupiter_data
 
     def load_names(self): #determing names for moons 
@@ -26,25 +33,30 @@ class Moons:
             
         return names_list
     
+    def moons_head(self):
+        return self.data.head()
+    
+    def drop_navalues(self, x):
+        dropna_df = self.data[self.data[x].notna()]
+        return dropna_df
+    
+    def data_summaries(self):
+        return self.data.describe()
+    
     def plot(self, x, y):
-        
-        self.data = self.data.apply(pd.to_numeric, errors='coerce')
-        corr_matrix = self.data.corr()
-        
-        plt.scatter(self.data[x], self.data[y], label=f'Correlation: {corr_matrix[x][y].round(3)}')
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.legend()
+        sns.relplot(self.data, x=x, y=y, hue="Group")
+        plt.title("Plot of Correlation Between Two Variables")
         plt.show()
         
-    def distributions(self):
-        self.data = self.data.apply(pd.to_numeric, errors='coerce')
-        number_columns = self.data.select_dtypes(['float64', 'int64']).columns
-
-        for col in number_columns:
-            sns.histplot(data=self.data, x=col, bins=20)
-            plt.show()
         
+    def distributions(self):
+        
+        number_columns = self.data.drop("Group", axis=1)
+        for col in number_columns:
+            sns.histplot(self.data, x=col, bins=5)
+            plt.title(f"Distribution of {col}")
+            plt.show()
+            
     
     def correlation_heatmap(self):
         
@@ -56,7 +68,7 @@ class Moons:
         plt.title('Correlation Heatmap')
         plt.show()
     
-    def correlation(self):
+    def correlation_value(self):
         self.data = self.data.apply(pd.to_numeric, errors='coerce')
         corr_matrix = self.data.corr()
         
@@ -64,19 +76,19 @@ class Moons:
         var_2 = input("Variable 2")
         
         if var_1 and var_2 in self.jupiter_data.iloc[0]:
-            variable_correlation = corr_matrix[var_1][var_2].round(3)
+            variable_correlation = print(f"The correlation value between {var_1} and {var_2} is {corr_matrix[var_1][var_2].round(3)}")
         
         return variable_correlation
 
         
     def column_average(self):
 
-        column_averages = {'period average' : [self.data["period_days"].mean(axis=0)],
-                           'distance average' : [self.data["distance_km"].mean(axis=0)],
-                           'radius average' : [self.data["radius_km"].mean(axis=0)],
-                           'magnitude average' : [self.data["mag"].mean(axis=0)],
-                           'eccentricity average' : [self.data["ecc"].mean(axis=0)],
-                           'inclination degree average' : [self.data["inclination_deg"].mean(axis=0)]}
+        column_averages = {'Period Average (days)' : [self.data["Period (days)"].mean(axis=0)],
+                           'Distance Average (km)' : [self.data["Distance (km)"].mean(axis=0)],
+                           'Radius Average (km)' : [self.data["Radius (km)"].mean(axis=0)],
+                           'Magnitude Average' : [self.data["Mangnitude"].mean(axis=0)],
+                           'Eccentricity Average' : [self.data["Eccentricity"].mean(axis=0)],
+                           'Inclination Degree Average (Degrees)' : [self.data["Inlination Degree"].mean(axis=0)]}
         
         averages_transposed = pd.DataFrame(column_averages).transpose()
         column_average_df = averages_transposed.rename(columns={0: "Average value"})
@@ -87,20 +99,23 @@ class Moons:
     
     def moon_info(self):
 
-            x = input("name of moon")
+            x = input("Name of moon for information summary")
             
             if x in self.jupiter_data.index:
                 
                 moon_info = pd.DataFrame(self.jupiter_data.loc[x])
                 return moon_info
+            else:
+                none = print("None")
+                return none
     
     def train(self):
         from sklearn.model_selection import train_test_split
         from sklearn.metrics import mean_squared_error, r2_score
         from sklearn import linear_model
         
-        self.data["radius_variable"] = (self.data["distance_km"]*1000)**3
-        self.data["period_variable"] = (self.data["period_days"]*86400)** 2
+        self.data["radius_variable"] = (self.data["Distance (km)"]*1000)**3
+        self.data["period_variable"] = (self.data["Period (days)"]*86400)** 2
         
         X = self.data[["radius_variable"]]
         Y = self.data["period_variable"]
@@ -123,14 +138,14 @@ class Moons:
         jupiter_mass = (4 * np.pi**2) / (model.coef_[0]*6.67e-11)
         print(f"The R2 score is: {r2_score(y_test,testing_pred_y)}")
         
-        training_residuals = y_train - training_pred_y
+        training_res = y_train - training_pred_y
         testing_residuals = y_test - testing_pred_y
-            
-        plt.scatter(x_train, training_residuals, color='blue', label='Training Residuals', alpha=0.5)
-        plt.scatter(x_test, testing_residuals, color='red', label='Testing Residuals')
+        
+        plt.scatter(x_train, training_res, color='red', label='Testing Residuals')
+        plt.title("Plot of Residuals on the Training set")
         plt.show()
        
-        return jupiter_mass
+        return print(f"The predicted masss of jupiter is {jupiter_mass}kg.")
     
     
  
